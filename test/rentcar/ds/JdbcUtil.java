0eaa788c68f632902632d21f50151657cb1aeec7
digi5633 <digi5633@gmail.com>
2020-10-05 17:26:51 +0900

    Member DaoImpl Test 끝!

--- test/rentcar/ds/JdbcUtil.java
+++ test/rentcar/ds/JdbcUtil.java
@@ -1,25 +1,21 @@
-package rentcar .ds;
+package rentcar.ds;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class JdbcUtil {
-    public static Connection getConnection() {
-        Connection conn = null;
-        try {
-        	String url = "jdbc:oracle:thin:@localhost:1521:orcl?useSSL=false";
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			String url = "jdbc:oracle:thin:@localhost:1521:orcl?useSSL=false";
 			String user = "RENTCAR";
 			String password = "rootroot";
 			conn = DriverManager.getConnection(url, user, password);
-        }catch (Exception e) {
-        	System.out.println(e.getMessage());
-        	e.printStackTrace();
-        }
-        return conn;
-    }
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return conn;
+	}
 
 }
