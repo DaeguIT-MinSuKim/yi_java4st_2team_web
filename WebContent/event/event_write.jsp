e686e994108c1ec6b4bd6096322a9fb3dfb29c19
Oh-Su-Jung <tnwjddl1996@gmail.com>
2020-10-14 17:13:01 +0900

    이벤트 글쓰기 완료

--- WebContent/event/event_write.jsp
+++ WebContent/event/event_write.jsp
@@ -11,21 +11,21 @@
 		<div class="location pc">HOME <span>></span> 이벤트 글쓰기</div>
 		
 		<div class="event_content">
-			<form action="eventWrite.do" method="post" class="event_write">
+			<form action="eventWrite.do" method="post" class="event_write" enctype="multipart/form-data">
 				<div>
-					<label><input type="checkbox" checked> 사용</label>
-					<input type="text" placeholder="제목을 입력해주세요" id="title">
-					<input type="text" placeholder="할인" id="sale" class="mt5">
+					<label><input type="checkbox" name="isEvent" checked> 사용</label>
+					<input type="text" name="title" placeholder="제목을 입력해주세요" id="title" value="test">
+					<input type="text" name="sale" placeholder="할인" id="sale" class="mt5" value="1000">
 					<div class="file_wrap">
-						<p>리스트 이미지</p><input type="file" class="list_file">
-						<p>상세페이지 이미지</p><input type="file" class="view_file">
+						<p>리스트 이미지</p><input type="file" name="uploadfile01" class="list_file">
+						<p>상세페이지 이미지</p><input type="file" name="uploadfile02" class="view_file">
 					</div>
 				</div>
 				
 	
 				<ul class="button_style3 mt50">
 					<li><a href="event.do" class="btn_small btn_case4">취소</a></li>
-					<li><a href="#" class="btn_small btn_case2" id="event_write">확인</a></li>
+					<li><input type="submit" value="확인" class="btn_small btn_case2" id="event_write"></li>
 				</ul>
 			</form>
 		</div>
@@ -54,8 +54,8 @@
 				alert("상세페이지 이미지를 첨부해주세요.")
 				return false;
 			}
-			$(".event_write").submit();
-			return false;
+			//$(".event_write").submit();
+			//return false;
 		})
 	})
 </script>
--- src/rentcar/controller/handler/event/EventWriteHandler.java
+++ src/rentcar/controller/handler/event/EventWriteHandler.java
@@ -13,8 +13,11 @@ import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import rentcar.controller.Command;
+import rentcar.dto.Event;
+import rentcar.service.EventService;
 
 public class EventWriteHandler implements Command {
+	private EventService service = new EventService();
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response)
@@ -33,10 +36,11 @@ public class EventWriteHandler implements Command {
 			int uploadFileSizeLimit = 5 * 1024 * 1024;
 			String enctype = "UTF-8";
 			
-			ServletContext context = request.getSession().getServletContext(); //컨트롤러 인터페이스를 상속받아 처리하는 경우 request.getSession() 사용
+			ServletContext context = request.getServletContext(); //컨트롤러 인터페이스를 상속받아 처리하는 경우 request 사용
 			String uploadFilePath  = context.getRealPath(savePath);
 			
 			try {
+				System.out.println(request);
 				MultipartRequest multi = new MultipartRequest(
 						request,
 						uploadFilePath,
@@ -45,20 +49,46 @@ public class EventWriteHandler implements Command {
 						new DefaultFileRenamePolicy()
 						);
 				Enumeration files = multi.getFileNames();
-				while (files.hasMoreElements()) {
+				
+				/*while (files.hasMoreElements()) {
 					String file = (String) files.nextElement();
 					String file_name = multi.getFilesystemName(file);
 					// 중복된 파일일 경우 파일명 변경이 됨
 					String ori_file_name = multi.getOriginalFileName(file); // 원본 파일명
 					
-					out.print("<br>업로드된 파일명 : "+ file_name);
-					out.print("<br>원본 파일명 : "+ ori_file_name);
-					out.print("<hr>");
+					System.out.println(file_name);
+					System.out.println(ori_file_name);
+				}*/
+				
+				String name = multi.getParameter("title");
+				int sale = Integer.parseInt(multi.getParameter("sale"));
+				String thumImage = multi.getFilesystemName("uploadfile01");
+				String viewImage = multi.getFilesystemName("uploadfile02");
+				String isEventArr[] = multi.getParameterValues("isEvent");
+				String isEvent;
+				
+				if (isEventArr == null) {
+					isEvent = "n";
+				} else {
+					isEvent = "y";
 				}
+				
+				Event event = new Event();
+				event.setName(name);
+				event.setSale(sale);
+				event.setThumImage(thumImage);
+				event.setViewImage(viewImage);
+				event.setIsEvent(isEvent);
+				
+				System.out.println(event);
+				service.insertEvent(event);
+				
+				
 			} catch (Exception e) {
 				System.out.println("예외 발생 : "+ e);
 			}
 			
+			response.sendRedirect("event.do");
 			return null;
 		}
 	}
