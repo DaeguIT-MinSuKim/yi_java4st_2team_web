abc8c0230298035f6c96647dee8e6ffaea264f0a
Oh-Su-Jung <tnwjddl1996@gmail.com>
2020-10-18 17:11:19 +0900

    관리자 작업

---
+++ WebContent/admin/include/footer.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!-- 하단 -->
+<footer>
+	<p>Copyright © 2020 All rights reserved by  김대현, 오수정, 문중희, 이승은, 김창동.</p>
+</footer>
+
+</body>
+</html>
---
+++ src/rentcar/controller/handler/admin/AdminHandler.java
@@ -0,0 +1,19 @@
+package rentcar.controller.handler.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import rentcar.controller.Command;
+
+public class AdminHandler implements Command {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		return "/admin/admin.jsp";
+	}
+
+}
