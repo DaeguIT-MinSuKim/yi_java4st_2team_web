573f17acf491eb9857b9b7c7bcad0d491d7f00c0
digi5633 <digi5633@gmail.com>
2020-10-19 18:03:50 +0900

    로그인 횟수 제한 성공!!

---
+++ src/rentcar/dao/impl/LoginFailDaoImpl.java
@@ -0,0 +1,109 @@
+package rentcar.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import rentcar.dao.LoginFailDao;
+import rentcar.dto.Member;
+
+public class LoginFailDaoImpl implements LoginFailDao {
+
+	private static final LoginFailDaoImpl instance = new LoginFailDaoImpl();
+	private Connection con;
+
+	public LoginFailDaoImpl() {
+		super();
+	}
+
+	public static LoginFailDaoImpl getInstance() {
+		return instance;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+
+	@Override
+	public int loginLockStatus(Member member) {
+		ResultSet rs = null;
+		String sql = "SELECT COUNT(1) CNT\r\n"
+				+ "  FROM MEMBER\r\n"
+				+ " WHERE ID = ?\r\n"
+				+ "   AND IS_LOCK = 'Y'\r\n"
+				+ "   AND LOGIN_DATE + ( 1 / 24 / 60 ) * LOCK_COUNTING > SYSDATE";
+		try (PreparedStatement pstmt = con.prepareStatement(sql);) {
+			pstmt.setString(1, member.getId());
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt("CNT");
+			}
+			return 0;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	public int loginFailCount(Member member) {
+		String sql = "UPDATE MEMBER\r\n"
+				+ "   SET TRY_COUNTING = TRY_COUNTING + 1, LOGIN_DATE = SYSDATE\r\n"
+				+ " WHERE ID = ?";
+		try (PreparedStatement pstmt = con.prepareStatement(sql);) {
+			pstmt.setString(1, member.getId());
+			return pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	public int resetLoginFailCount(Member member) {
+		String sql = "UPDATE MEMBER\r\n"
+				+ "   SET TRY_COUNTING = 0, IS_LOCK = 'N'\r\n"
+				+ " WHERE ID = ?\r\n"
+				+ "   AND IS_LOCK = 'Y'";
+		try (PreparedStatement pstmt = con.prepareStatement(sql);) {
+			pstmt.setString(1, member.getId());
+			return pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	public int loginLock(Member member) {
+		String sql = "UPDATE MEMBER\r\n"
+				+ "   SET IS_LOCK = 'Y', LOCK_COUNTING = LOCK_COUNTING + 1\r\n"
+				+ " WHERE ID = ?\r\n"
+				+ "   AND TRY_COUNTING > 3";
+		try (PreparedStatement pstmt = con.prepareStatement(sql);) {
+			pstmt.setString(1, member.getId());
+			return pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	public int resetLockCount(Member member) {
+		String sql = "UPDATE MEMBER\r\n"
+				+ "   SET LOCK_COUNTING = 0\r\n"
+				+ " WHERE ID = ?";
+		try (PreparedStatement pstmt = con.prepareStatement(sql);) {
+			pstmt.setString(1, member.getId());
+			return pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+}
---
+++ test/rentcar/dao/impl/LoginFailDaoImplTest.java
@@ -0,0 +1,114 @@
+package rentcar.dao.impl;
+
+import java.sql.Connection;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import rentcar.dao.LoginFailDao;
+import rentcar.ds.JdbcUtil;
+import rentcar.dto.Member;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class LoginFailDaoImplTest {
+
+	private static Connection con;
+	private LoginFailDao dao;
+
+	@Before
+	public void setUp() throws Exception {
+		con = JdbcUtil.getConnection();
+		dao = LoginFailDaoImpl.getInstance();
+		((LoginFailDaoImpl) dao).setCon(con);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		dao = null;
+	}
+
+	@Test
+	public void test06LoginLockStatus() {
+		System.out.printf("%s()%n", "test06LoginLockStatus");
+		Member loginFail = new Member("dong");
+		int res = LoginFailDaoImpl.getInstance().loginLockStatus(loginFail);
+		Assert.assertEquals(1, res);
+
+		System.out.printf("%s%n%n", loginFail);
+	}
+
+	@Test
+	public void test01LoginFailCount() {
+		System.out.printf("%s()%n", "test01LoginFailCount");
+		Member loginFail = new Member("dong");
+		int res = LoginFailDaoImpl.getInstance().loginFailCount(loginFail);
+		Assert.assertEquals(1, res);
+
+		System.out.printf("%s%n%n", loginFail);
+	}
+
+	@Test
+	public void test02LoginFailCount() {
+		System.out.printf("%s()%n", "test02LoginFailCount");
+		Member loginFail = new Member("dong");
+		int res = LoginFailDaoImpl.getInstance().loginFailCount(loginFail);
+		Assert.assertEquals(1, res);
+
+		System.out.printf("%s%n%n", loginFail);
+	}
+
+	@Test
+	public void test03LoginFailCount() {
+		System.out.printf("%s()%n", "test03LoginFailCount");
+		Member loginFail = new Member("dong");
+		int res = LoginFailDaoImpl.getInstance().loginFailCount(loginFail);
+		Assert.assertEquals(1, res);
+
+		System.out.printf("%s%n%n", loginFail);
+	}
+
+	@Test
+	public void test04LoginFailCount() {
+		System.out.printf("%s()%n", "test04LoginFailCount");
+		Member loginFail = new Member("dong");
+		int res = LoginFailDaoImpl.getInstance().loginFailCount(loginFail);
+		Assert.assertEquals(1, res);
+
+		System.out.printf("%s%n%n", loginFail);
+	}
+
+	@Test
+	public void test07ResetLoginFailCount() {
+		System.out.printf("%s()%n", "test07ResetLoginFailCount");
+		Member loginFail = new Member("dong");
+		int res = LoginFailDaoImpl.getInstance().resetLoginFailCount(loginFail);
+		Assert.assertEquals(1, res);
+
+		System.out.printf("%s%n%n", loginFail);
+	}
+
+	@Test
+	public void test05LoginLock() {
+		System.out.printf("%s()%n", "test05LoginLock");
+		Member loginFail = new Member("dong");
+		int res = LoginFailDaoImpl.getInstance().loginLock(loginFail);
+		Assert.assertEquals(1, res);
+
+		System.out.printf("%s%n%n", loginFail);
+	}
+
+	@Test
+	public void test08ResetLockCount() {
+		System.out.printf("%s()%n", "test08ResetLockCount");
+		Member loginFail = new Member("dong");
+		int res = LoginFailDaoImpl.getInstance().resetLockCount(loginFail);
+		Assert.assertEquals(1, res);
+
+		System.out.printf("%s%n%n", loginFail);
+	}
+
+}
