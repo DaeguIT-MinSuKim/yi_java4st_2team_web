2d6bb9cf34539f387f46dfe1734e8842da1f6ab9
Oh-Su-Jung <tnwjddl1996@gmail.com>
2020-10-21 16:52:17 +0900

    관리자 이벤트 게시판 페이징 완료

--- src/rentcar/controller/handler/admin/event/AdminEventHandler.java
+++ src/rentcar/controller/handler/admin/event/AdminEventHandler.java
@@ -19,7 +19,7 @@ public class AdminEventHandler implements Command {
 	public String process(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String nowPage = request.getParameter("nowPage"); // 현재 페이지
-		String cntPerPage = "1"; // 페이지당 몇 개의 글을 보일지 저장
+		String cntPerPage = "15"; // 페이지당 몇 개의 글을 보일지 저장
 		
 		// 처음 접속했을 때 초기 세팅
 		int total = service.countEventByall();
