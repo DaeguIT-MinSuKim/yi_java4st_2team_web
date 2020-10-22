2a98d7969870c6fc7f450c739535aa42f1f56233
seungeun <tmddms0931@naver.com>
2020-10-22 14:21:23 +0900

    update commit

--- src/rentcar/controller/handler/admin/notice/AdminNoticeUpdateHandler.java
+++ src/rentcar/controller/handler/admin/notice/AdminNoticeUpdateHandler.java
@@ -10,32 +10,32 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 import rentcar.controller.Command;
-import rentcar.dto.LongRent;
-import rentcar.service.LongRentSerivce;
+import rentcar.dto.Notice;
+import rentcar.service.NoticeSerivce;
 
 public class AdminNoticeUpdateHandler implements Command {
-	private LongRentSerivce service = new LongRentSerivce();
+	private NoticeSerivce service = new NoticeSerivce();
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 	
-//		if(request.getMethod().equalsIgnoreCase("get")) {
-//			System.out.println("UpdateModel Get!!!");
-//			
-//			LongRent longrent = service.getLongRent(Integer.parseInt(request.getParameter("no")));
-//			System.out.println(longrent);
-//			request.setAttribute("LongRent", longrent);
-//			
-//			return "/longrent/longrent_update.jsp";
-//		}else {
-//			Gson gson= new Gson();
-//			LongRent longrent = gson.fromJson(new InputStreamReader(request.getInputStream(), "UTF-8"), LongRent.class);
-//			System.out.println("update >> "+ longrent);
-//			
-//			int res = service.updateLongRent(longrent);
-//			response.getWriter().print(res);
+		if(request.getMethod().equalsIgnoreCase("get")) {
+			System.out.println("UpdateModel Get!!!");
+			
+			Notice notice = service.getNotice(Integer.parseInt(request.getParameter("no")));
+			System.out.println(notice);
+			request.setAttribute("Notice", notice);
+			
+			return "admin/notice/notice_update.jsp";
+		}else {
+			Gson gson= new Gson();
+			Notice notice = gson.fromJson(new InputStreamReader(request.getInputStream(), "UTF-8"), Notice.class);
+			System.out.println("update >> "+ notice);
+			
+			int res = service.updateNotice(notice);
+			response.getWriter().print(res);
 			return null;
 		}
-//	}
+	}
 }
